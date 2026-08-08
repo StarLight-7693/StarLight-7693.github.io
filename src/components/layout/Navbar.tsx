f1c3b2e5c2e9d18fd1a01c7cacfdc4a726bafee3
StarLight-7693 <kl1319700698@163.com>
2026-08-09 00:09:56 +0800

    修改B站社交链接

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
         }}>首页</a>
         <a href={`${window.location.origin}/blog`}>博客</a>
         <a href="https://github.com/StarLight-7693" target="_blank">GitHub</a>
-        <a href="https://space.bilibili.com/3707028635650857" target="_blank">B站</a>
+        <a href="https://space.bilibili.com/3632301571311734" target="_blank">B站</a>
       </div>
     </nav>
   )
